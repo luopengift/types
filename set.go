f6dfe3949f9f1047e252984212c72fec4398a951
luopeng <luopeng@xhqb.com>
2018-08-16 18:13:47 +0800

    update set

--- set.go
+++ set.go
@@ -4,87 +4,83 @@ import (
 	"sync"
 )
 
-// Set set
-type Set struct {
-	s   map[interface{}]bool
-	mux *sync.RWMutex
+// Setter set interface
+type Setter interface {
+	Add(...interface{}) Setter
+	Remove(interface{}) Setter
+	Contains(interface{}) bool
+	Elements() []interface{}
+	//Equals(Set) bool
+	Len() int
+	//Copy() Set
+	//Sub(Set) Set
+	Diff(Setter) Setter
+	Inter(Setter) Setter
+	Union(Setter) Setter
+	Same(Setter) bool
+	Clear() error
 }
 
-// NewSet new set
-func NewSet(e ...interface{}) *Set {
-	set := Set{
-		s:   make(map[interface{}]bool),
-		mux: new(sync.RWMutex),
-	}
-	set.Add(e...)
-	return &set
-}
+// UnsafeSet unsafe set
+type UnsafeSet map[interface{}]bool
 
-// Add 添加一个元素
-func (s *Set) Add(e ...interface{}) *Set {
-	s.mux.Lock()
+// Add add elements
+func (s UnsafeSet) Add(e ...interface{}) Setter {
 	for _, v := range e {
-		s.s[v] = true
+		s[v] = true
 	}
-	s.mux.Unlock()
 	return s
 }
 
+// Contains contains
+func (s UnsafeSet) Contains(v interface{}) bool {
+	return s[v]
+}
+
 // Remove 删除一个元素
-func (s *Set) Remove(v interface{}) *Set {
-	s.mux.Lock()
-	delete(s.s, v)
-	s.mux.Unlock()
+func (s UnsafeSet) Remove(v interface{}) Setter {
+	delete(s, v)
 	return s
 }
 
 // Clear 清空所有元素
-func (s *Set) Clear() error {
-	s.mux.Lock()
-	for k := range s.s {
+func (s UnsafeSet) Clear() error {
+	for k := range s {
 		s.Remove(k)
 	}
-	s.mux.Unlock()
 	return nil
 }
 
-// Contains contains
-func (s *Set) Contains(v interface{}) bool {
-	s.mux.RLock()
-	defer s.mux.RUnlock()
-	return s.s[v]
-}
-
 // Elements 获取元素集合
-func (s *Set) Elements() []interface{} {
+func (s UnsafeSet) Elements() []interface{} {
 	elements := []interface{}{}
-	for k := range s.s {
+	for k := range s {
 		elements = append(elements, k)
 	}
 	return elements
 }
 
-// Len len
-func (s *Set) Len() int {
-	return len(s.s)
-}
-
 // Same 是否和其他set一致
-func (s *Set) Same(o *Set) bool {
-	if s.Len() != o.Len() {
+func (s UnsafeSet) Same(set Setter) bool {
+	if s.Len() != set.Len() {
 		return false
 	}
-	for k := range s.s {
-		if !o.Contains(k) {
+	for k := range s.Elements() {
+		if !set.Contains(k) {
 			return false
 		}
 	}
 	return true
 }
 
+// Len len
+func (s UnsafeSet) Len() int {
+	return len(s)
+}
+
 // Union 并集
-func (s *Set) Union(o *Set) *Set {
-	union := NewSet(s.Elements()...) //新创建一个集合,以免影响原集合
+func (s UnsafeSet) Union(o Setter) Setter {
+	union := NewSafeSet(s.Elements()...) //新创建一个集合,以免影响原集合
 	for _, v := range o.Elements() {
 		union.Add(v)
 	}
@@ -92,8 +88,8 @@ func (s *Set) Union(o *Set) *Set {
 }
 
 // Inter 交集
-func (s *Set) Inter(o *Set) *Set {
-	inter := NewSet()
+func (s UnsafeSet) Inter(o Setter) Setter {
+	inter := NewUnsafeSet()
 	for _, v := range s.Elements() {
 		if o.Contains(v) {
 			inter.Add(v)
@@ -103,12 +99,105 @@ func (s *Set) Inter(o *Set) *Set {
 }
 
 // Diff 差集
-func (s *Set) Diff(o *Set) *Set {
-	diff := NewSet()
+func (s UnsafeSet) Diff(o Setter) Setter {
+	diff := NewUnsafeSet()
 	for _, v := range s.Elements() {
 		if !o.Contains(v) {
 			diff.Add(v)
 		}
 	}
 	return diff
+}
+
+// NewUnsafeSet unsafe set
+func NewUnsafeSet(e ...interface{}) Setter {
+	set := new(UnsafeSet)
+	set.Add(e...)
+	return set
+}
+
+// SafeSet set
+type SafeSet struct {
+	s   Setter
+	mux *sync.RWMutex
+}
+
+// NewSafeSet new set
+func NewSafeSet(e ...interface{}) Setter {
+	set := SafeSet{
+		s:   NewUnsafeSet(e...),
+		mux: new(sync.RWMutex),
+	}
+	return &set
+}
+
+// Add 添加一个元素
+func (s *SafeSet) Add(e ...interface{}) Setter {
+	s.mux.Lock()
+	s.s.Add(e...)
+	s.mux.Unlock()
+	return s
+}
+
+// Remove 删除一个元素
+func (s *SafeSet) Remove(v interface{}) Setter {
+	s.mux.Lock()
+	s.s.Remove(v)
+	s.mux.Unlock()
+	return s
+}
+
+// Clear 清空所有元素
+func (s *SafeSet) Clear() error {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	return s.s.Clear()
+}
+
+// Contains contains
+func (s *SafeSet) Contains(v interface{}) bool {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
+	return s.s.Contains(v)
+}
+
+// Elements 获取元素集合
+func (s *SafeSet) Elements() []interface{} {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
+	return s.s.Elements()
+
+}
+
+// Len len
+func (s *SafeSet) Len() int {
+	return s.s.Len()
+}
+
+// Same 是否和其他set一致
+func (s *SafeSet) Same(set Setter) bool {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
+	return s.s.Same(set)
+}
+
+// Union 并集
+func (s *SafeSet) Union(o Setter) Setter {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
+	return s.s.Union(o)
+}
+
+// Inter 交集
+func (s *SafeSet) Inter(set Setter) Setter {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
+	return s.s.Inter(set)
+}
+
+// Diff 差集
+func (s *SafeSet) Diff(set Setter) Setter {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
+	return s.s.Diff(set)
 }
--- set_test.go
+++ set_test.go
@@ -7,12 +7,12 @@ import (
 
 func Test_Set(t *testing.T) {
 	fmt.Println("[" + t.Name() + "]")
-	set1 := NewSet(1, "2", "3")
+	set1 := NewSafeSet(1, "2", "3")
 	set1.Add(1)
 	fmt.Printf("  %v\n", set1.Contains(1))
-	set2 := NewSet("1", "2", "3", "4")
+	set2 := NewSafeSet("1", "2", "3", "4")
 	fmt.Printf("  %v %v\n", set1, set2)
-	set3 := NewSet()
+	set3 := NewSafeSet()
 	fmt.Printf("  2-1 %v\n", set2.Diff(set1))
 	fmt.Printf("  1-2 %v\n", set1.Diff(set2))
 	fmt.Printf("  %v\n", set1.Diff(set3))
